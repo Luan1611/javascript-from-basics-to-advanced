f22c960fcc75f90ff11c51dc9938c8ee124da366
Luan1611 <luan_1611@hotmail.com>
2024-11-21 15:47:08 -0300

    feat: fifth arrays/objects exercise

---
+++ exercicios/exercicios_sobre_arrays_e_objetos/ex_05/script.js
@@ -0,0 +1,8 @@
+const names = ['Maria', 'Carlos', 'José', 'Luan', 'Helena']
+const myName = 'Luan'
+
+if (names.includes(myName)) {
+    console.log(`'${myName}' existe no array.`)
+} else {
+    console.log(`'${myName}' não existe no array.`)
+}
