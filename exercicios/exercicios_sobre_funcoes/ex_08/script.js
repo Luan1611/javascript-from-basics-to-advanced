972b5ff7a4ebe3f3d4b66311b2f1646218385a93
Luan1611 <luan_1611@hotmail.com>
2024-11-21 09:55:28 -0300

    feat: nineth function exercise

--- exercicios/exercicios_sobre_funcoes/ex_08/script.js
+++ exercicios/exercicios_sobre_funcoes/ex_08/script.js
@@ -2,3 +2,6 @@ const checkStringLength = string =>
     string.length > 10
         ? 'too long text' 
         : 'text inside limit'
+
+console.log(checkStringLength('Luan'))
+console.log(checkStringLength('São Carlos - SP'))
---
+++ exercicios/exercicios_sobre_funcoes/ex_09/script.js
@@ -0,0 +1,5 @@
+const calculateNumberPower = (basis, exponent) => basis ** exponent
+
+console.log(calculateNumberPower(3, 3))
+console.log(calculateNumberPower(4, 4))
+console.log(calculateNumberPower(2, 10))
